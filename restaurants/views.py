6355abeec7b6f12c425e1bdf16366cb817c4ba9c
Xeryto <igoshindanya@mail.ru>
2023-10-05 17:26:41 -0400

    small changes to connection string

--- restaurants/views.py
+++ restaurants/views.py
@@ -5,7 +5,7 @@ import certifi
 
 
 def index(request):
-    CONNECTION_STRING = "mongodb+srv://igoshindanya:632Aa5223@cluster0.fuho1fb.mongodb.net/Cluster0"
+    CONNECTION_STRING = "mongodb+srv://igoshindanya:632Aa5223@cluster0.fuho1fb.mongodb.net/?retryWrites=true&w=majority"
 
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
     client = MongoClient(CONNECTION_STRING, tlsCAFile=certifi.where())
