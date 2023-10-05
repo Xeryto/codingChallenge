2f4ab68209db752a9a8a61c3bd5772c8b1a5f89f
Xeryto <igoshindanya@mail.ru>
2023-10-05 10:21:13 -0400

    test mongodb connection

---
+++ restaurants/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+import certifi
+
+
+def index(request):
+    CONNECTION_STRING = "mongodb+srv://igoshindanya:632Aa5223@cluster0.fuho1fb.mongodb.net/Cluster0"
+
+    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+    client = MongoClient(CONNECTION_STRING, tlsCAFile=certifi.where())
+
+    print(client['sample_restaurants'].list_collection_names())
+
+    return HttpResponse("hello")
