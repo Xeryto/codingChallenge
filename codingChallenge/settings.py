ba1e6f4c30eee84a6ae76ed73e8f5ce075acea53
Xeryto <igoshindanya@mail.ru>
2023-10-06 00:42:31 -0400

    bootstrap + base.html integrated

--- codingChallenge/settings.py
+++ codingChallenge/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'restaurants.apps.RestaurantsConfig'
+    'restaurants.apps.RestaurantsConfig',
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
